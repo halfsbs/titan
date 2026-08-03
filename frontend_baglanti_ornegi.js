8196c2cf76a59f163c2f94ed76f0036bb123a15e
halfsbs <46484974+halfsbs@users.noreply.github.com>
2026-08-03 18:45:02 +0300

    Delete frontend_baglanti_ornegi.js

--- frontend_baglanti_ornegi.js
+++
@@ -1,9 +0,0 @@
-// Frontend için bunu en üste ekle
-const API_URL = localStorage.getItem('SUNGATE_API_URL') || 'hthttps://titan-djmk.onrender.com';
-
-// Kullanım örneği:
-fetch(`${API_URL}/check-cccam`, {
-  method: 'POST',
-  headers: {'Content-Type': 'application/json'},
-  body: JSON.stringify({lines: clineList, timeout: 5, delay: 1})
-})
